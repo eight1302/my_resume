6392eea531462ddaf98e129c780375f2ae04cc6b
Eight <380080496@qq.com>
2018-12-12 12:57:07 +0800

    Update react_resune.js

--- my_resume/js/react_resune.js
+++ my_resume/js/react_resune.js
@@ -8,7 +8,7 @@ new Vue({
     jobtimes : '2016-10',
     job : '求职意向：前端工程师',
     salaryname : '薪资',
-    salary : '14~16',
+    salary : '12.5~14.5',
     jobtimesale : '',
     education_ba : '教育背景',
     education : [{
